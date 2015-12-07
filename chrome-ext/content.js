b6d63ce97c9b98b4cb49563a0a8859bbbae1de7b
Cloé <cloe.duc@gmail.com>
2015-12-07 22:00:30 +0100

    remove easter egg

--- chrome-ext/content.js
+++ chrome-ext/content.js
@@ -8,18 +8,4 @@ $(document).ready(function(){
 			$(this).css('user-select', 'text');
 		});
 	});
-	var listenWord = "";
-	var desired_word = "42108111118108121";
-	$(document).keypress(function(e) {
-		var stringtoadd = ""+e.which;
-		listenWord+=stringtoadd;
-		if(listenWord==desired_word){
-			$('.panel-reading').each(function(){
-					$(this).css('background-color', 'blue');
-			});
-			$('.panel.panel-reading pre').each(function(){
-				$(this).css('color', 'red');
-			});
-		}
-	});
 })
